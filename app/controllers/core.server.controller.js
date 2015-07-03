783115d63327d8698313c0d830db510b06f62a37
kyle-rader <kyle.rader@ieee.org>
2015-07-02 21:13:54 -0700

    Implement new email subscribe functionality.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -17,19 +17,48 @@ exports.index = function(req, res) {
 };
 
 exports.emailSubscribe = function(req, res) {
-	var validEmail = validator.validate(req.body.email);
+
+	var msg = null;
+	if (!req.body.fname || (req.body.fname && req.body.fname.length === 0)) {
+		msg = {message: 'A first name is required'};
+	} else if (!req.body.lname || (req.body.lname && req.body.lname.length === 0)) {
+		msg = {message: 'A last name is required'};
+	} else if (!req.body.emailAddr || (req.body.emailAddr && req.body.emailAddr.length === 0)) {
+		msg = {message: 'An email address is required'};
+	}
+	if (msg !== null)
+		return res.status(400).send(msg);
+
+	var validEmail = validator.validate(req.body.emailAddr);
+	var mcData = {
+		id: req.params.listId,
+		email: {email:req.body.emailAddr},
+		merge_fields: {
+			FNAME: req.body.fname,
+			LNAME: req.body.lname
+		},
+		interests: {
+			'a6c111d220': req.body.frontEndWebDev,
+			'5a9a0ceb8e': req.body.backEndWebDev,
+			'f00bf5af31': req.body.dbForDev,
+			'11de21f773': req.body.dbAdmin,
+			'0035c371bf': req.body.dbForNonDev,
+			'3f32d7d9de': req.body.forKids,
+			'62de2aa30f': req.body.idk
+		}
+	};
 
 	if (process.env.NODE_ENV === 'production' && validEmail) {
-		mc.lists.subscribe({id: req.params.listId, email:{email:req.params.email}},
+		mc.lists.subscribe(mcData,
 		function(data) {
-			res.send({email: req.params.eamil});
+			res.send({message: 'Thank you <strong>' + req.body.fname + '</strong>!<br>Your email (<strong>' + req.body.emailAddr + '</strong>) has been subscribed!'});
 		},
 		function(error) {
-			res.send({email: null, error: 'There was an error subscribing that email.\n' + error.error});
+			res.send({email: req.body.emailAddr, message: 'There was an error subscribing that email.\n' + error.error});
 		});
 	} else if (process.env.NODE_ENV === 'development') {
-		res.send({email: null, error: 'This email "' + req.params.email + '" would have been subscribe - but you are hitting a dev server.'});
+		res.send({email: req.body.emailAddr, message: 'This email <strong>' + req.body.emailAddr + '</strong> would have been subscribed - but you are hitting a dev server!'});
 	} else {
-		res.send({email: null, error: 'Invalid email address format'});
+		res.send({email: req.body.emailAddr, message: 'Invalid email address format'});
 	}
 };
--- public/modules/core/controllers/mailing-list-signup.client.controller.js
+++ public/modules/core/controllers/mailing-list-signup.client.controller.js
@@ -1,7 +1,19 @@
 'use strict';
 
-angular.module('core').controller('MailingListSignupController', ['$scope',
-	function($scope) {
-		
+angular.module('core').controller('MailingListSignupController', ['$scope', '$http', '$location',
+	function($scope, $http, $location) {
+
+		$scope.subscribeEmail = function() {
+			$http.post('/mail/subscribe/d7142be008', $scope.user)
+				.success(function(response) {
+					delete $scope.user;
+					$scope.mailingListForm.$setPristine();
+					$scope.success = response.message;
+					delete $scope.error;
+				})
+				.error(function(error) {
+					$scope.error = error.message;
+				});
+		};
 	}
 ]);
